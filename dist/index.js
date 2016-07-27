6efb208a3bfb740be828754e84e4128a4a9010f9
Tom Tornese <tornese.t@husky.neu.edu>
2016-07-27 15:44:26 -0400

    re-adding dist gulp task

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-/* eslint-disable no-undef, no-console */
+var _Button=require('./Button/Button.react');var _Button2=_interopRequireDefault(_Button);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
 
-var _ButtonButtonReact = require('./Button/Button.react');
-
-var _ButtonButtonReact2 = _interopRequireDefault(_ButtonButtonReact);
-
-module.exports = exports = {};
-exports.Button = _ButtonButtonReact2.default;
+module.exports=exports={};
+exports.Button=_Button2.default;
